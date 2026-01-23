90b953ada7d9fae847231919fd52b32d8d699036
koeurkys <arai.hivanui@gmail.com>
2026-01-22 17:11:42 -1000

    Réorganisation des routes : suppression de la redondance pour la route de résumé des transactions.

--- src/routes/transactionsRoute.js
+++ src/routes/transactionsRoute.js
@@ -3,15 +3,12 @@ import express from "express";
 import {getTransactionsByUserId, createTransaction, deleteTransaction, getSummaryByUserId } from "../controllers/transactionsController.js";
 const router = express.Router();
 
-
+router.get("/summary/:userId", getSummaryByUserId);
 
 router.get("/:userId", getTransactionsByUserId);
-
 
 router.post("", createTransaction);
 
 router.delete("/:id", deleteTransaction);
-
-router.get("/summary/:userId", getSummaryByUserId);
 
 export default router;
